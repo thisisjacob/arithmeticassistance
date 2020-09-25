dd2d2855ec17291b3699129b3a6fee8b997411e0
jacob h <42303925+thisisjacob@users.noreply.github.com>
2020-09-25 17:11:06 -0400

    fixed previous update

--- ui/mainWindow.rkt
+++ ui/mainWindow.rkt
@@ -10,11 +10,21 @@
     (super-new)
     (define mainFrame (new frame% [label title]))
     (define topMenuBar (new menuBar% [menuParent mainFrame]))
+    (define menuOptions (new panel%
+                             [parent mainFrame]
+                             [style '(border)]
+                             [horiz-margin 100]
+                             [vert-margin 100]
+                             [enabled #t]))
+    (define testButton1 (new button% [label "Test"] [parent menuOptions])) ; remove these buttons later in development
+    (define testButton2 (new button% [label "Test"] [parent menuOptions])) ; current buttons all overlap with eachother - figure out how to do layouts
+    (define testButton3 (new button% [label "Test"] [parent menuOptions]))
     (define/public (openWindow)
       (send mainFrame show #t)
       (send topMenuBar activate))
     ))
 
 (provide mainWindow%)
+
 
 
