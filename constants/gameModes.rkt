a035354994700f88d31df583ee9ca0443958ede3
jacob h <42303925+thisisjacob@users.noreply.github.com>
2020-10-10 20:47:58 -0400

    Added a list definition for available gamemodes in a constant file

--- constants/gameModes.rkt
+++ constants/gameModes.rkt
@@ -9,20 +9,23 @@
             )
            )
   )
-
-(define game-mode-manager%
-  (class object%
-    (super-new)
-    (init-field
-     mode-list
-     )
-    )
-  )
-
-(define free (new game-mode%
                   
 
+(define game-modes (
+                    (new game-mode%
+                         [id 0]
+                         [name "Free Practice"]
+                         )
+                    (new game-mode%
+                         [id 1]
+                         [name "Linked Mode"]
+                         )
+                    (new game-mode%
+                         [id 2]
+                         [name "Multiplayer"]
+                         )
+                    )
+  )
 
 
-
-
+(provide game-modes)
