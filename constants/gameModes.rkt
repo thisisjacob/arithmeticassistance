e0550c31189572c029e882ceeb70305f0802ee5e
jacob h <42303925+thisisjacob@users.noreply.github.com>
2020-10-10 20:52:52 -0400

    Added documentation to gameModes.rkt, added needed retrieval functions

--- constants/gameModes.rkt
+++ constants/gameModes.rkt
@@ -1,4 +1,7 @@
 #lang racket
+; Defines a small class for holding information on a game mode
+; Defines a list named game-modes that is is just a list of available gamemodes for the program
+; This class should be used to hold information on what is available - it will not implement these gamemodes
 
 (define game-mode%
          (class object%
@@ -7,11 +10,17 @@
             id
             name
             )
+           (define/public (getId)
+             id
+             )
+           (define/public (getName)
+             name
+             )
            )
   )
                   
 
-(define game-modes (
+(define game-modes (list
                     (new game-mode%
                          [id 0]
                          [name "Free Practice"]
