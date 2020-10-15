43db706a36decd934fb61501a0bd8eeb40143a9c
jacob h <42303925+thisisjacob@users.noreply.github.com>
2020-10-14 20:07:35 -0400

    Cleaned up mainMenu to remove button creators

--- ui/mainMenu.rkt
+++ ui/mainMenu.rkt
@@ -1,5 +1,6 @@
 #lang racket
 (require racket/gui/base)
+(require "../utilities/buttonFunctionsAndGenerators.rkt")
 
 ; A class for creating the main menu of the program
 ; Instance Parameters:
@@ -20,32 +21,9 @@
     (define mainMenu (new panel%
       [parent givenParent]
      ))
+    
     (define menuWrapper (new panel%
                              [parent mainMenu]))
-
-    ; Event handlers
-    
-    ; Calls the function that is paired with the name of the button that fired the event
-    ; (using the string-function-pair-list field)
-    (define (button-handler button event)
-      (button-handler-helper button string-function-pair-list)
-      )
-
-    ; button-handler helper
-    ; Iterates through the given-string-function-pair-list, fires the function
-    ; that is paired with the label of the button that called button-handler in the list
-    ; First argument is the button that fired the event
-    ; Second argument is the string-function-pair-list of the current mainMenu instance
-    
-    (define (button-handler-helper button given-string-function-pair-list)
-      (cond
-        [(empty? given-string-function-pair-list) 0]
-        [(equal? (send button get-label) (first (first given-string-function-pair-list)))
-         ((first (rest (first given-string-function-pair-list))))]
-        [else (button-handler-helper button (rest given-string-function-pair-list))]
-        )
-      )
-     
       
     (define menu (new vertical-panel%
                       [style '(border)]
@@ -57,38 +35,14 @@
      (define header (new message%
                  [parent menu]
                  [label "Main Menu"]))
+
+    ; the button manager for managing menu's buttons
+    (define buttons (new buttonGeneratorAndManager%
+                         [givenParent menu]
+                         [string-function-pair-list string-function-pair-list]
+                         )
+      )
     
-    ; Private syntax definitions
-
-    ; Creates an anonymous button%
-    ; name is the label of the button
-    ; Function is the function for the button
-    ; Parent is the parent container to attach the button to
-    (define-syntax create-button
-      (syntax-rules ()
-              [(create-button name function parent)
-               (new button%
-                                 [callback button-handler]
-                                 [label name]
-                                 [parent parent])]))
-
-    ; Private Functions
-
-    ; Initializes buttons, attaches them to a container
-    ; First input is a list of pairs of strings (for the label of the button) and functions
-    ; Attaches the buttons to the given parent
-    (define (initialize-buttons given-string-function-pair-list parent)
-      (cond
-        [(empty? given-string-function-pair-list) 0]
-        [else (create-button (first (first given-string-function-pair-list))
-                             (rest (first given-string-function-pair-list))
-                             parent)
-              (initialize-buttons (rest given-string-function-pair-list) parent)]))
-    
-
-
-    (initialize-buttons string-function-pair-list menu)
-
 
     ; Public Functions
     (define/public (enable)
