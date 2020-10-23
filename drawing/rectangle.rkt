6a6e68078f70eb20b95f0a75cb42001becafbf9d
jacob h <42303925+thisisjacob@users.noreply.github.com>
2020-10-22 21:10:22 -0400

    Rectangle documentation

--- drawing/rectangle.rkt
+++ drawing/rectangle.rkt
@@ -1,20 +1,21 @@
 #lang racket
 (require racket/draw)
 (require "textProblem.rkt")
+(require "../constants/userInterfaceConstants.rkt")
 
 
-; Draws a circle onto the given device context
+; Draws a rectangle and a problem description onto the given device context
 ; Parameters:
 ; device-context: the device context
-; problem-radius: the radius in the math problem (for drawing text)
-; pixel-radius: the radius on the screen of the circle
-; x: x position
-; y: y position
+; xPos: the x position of the rectangle on the device context
+; yPos: the y position of the rectangle on the device context
+; problemWidth: the width of the rectangle within the problem
+; problemHeight: the height of the rectangle within the problem
+; problemDescription: a text description of the problem. positioned based on constants
 
-; TODO: Add text next to the circle for problem information
 (define (draw-rectangle device-context xPos yPos problemWidth problemHeight problemDescription)
   (send device-context draw-rectangle xPos yPos 50 50)
-  (draw-text-problem device-context problemDescription xPos (+ yPos 100))
+  (draw-text-problem device-context problemDescription problemTextPushRight problemTextPushdown)
   )
 
 
